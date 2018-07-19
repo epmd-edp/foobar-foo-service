f061cc88bf318c201373e13596627b0f5c082f3e
Pavlo Yemelianov <pavlo_yemelianov@epam.com>
2018-07-19 16:44:29 +0300

    [EPMDEDP-870] Remove configuration properties annotation
    Change-Id: Ia86a2e3c7b203bef4f438a1e4218df82ea83d68f

--- src/main/java/com/epam/edp/foo/FooApplication.java
+++ src/main/java/com/epam/edp/foo/FooApplication.java
@@ -2,10 +2,8 @@ package com.epam.edp.foo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@ConfigurationProperties
 @EnableFeignClients
 @SpringBootApplication
 public class FooApplication {
