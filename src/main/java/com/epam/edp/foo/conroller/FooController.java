656507b63f0e5898a87bd551703d7e038843171b
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2018-07-23 12:05:59 +0300

    Add host value to the response.
    Change-Id: Ic1f117b46ea4da4e7867ddf19a821025c71c8624

--- src/main/java/com/epam/edp/foo/conroller/FooController.java
+++ src/main/java/com/epam/edp/foo/conroller/FooController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author Pavlo_Yemelianov
  */
@@ -22,43 +25,51 @@ public class FooController {
 
     @GetMapping(value = "/api/config/default")
     public String getDefaultConfig() {
-        return configService.getDefaultConfig();
+        return configService.getDefaultConfig() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/config/profile")
     public String getProfileConfig() {
-        return configService.getProfileConfig();
+        return configService.getProfileConfig() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/config/mount")
     public String getMountConfig() {
-        return configService.getMountConfig();
+        return configService.getMountConfig() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/config/reload")
     public String getReloadConfig() {
-        return configService.getReloadConfig();
+        return configService.getReloadConfig() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/config/secret")
     public String getSecretConfig() {
-        return configService.getSecretConfig();
+        return configService.getSecretConfig() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/rpc/dumb-client")
     public String callDumbClient() {
-        return rpcService.callDumbClient();
+        return rpcService.callDumbClient() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/rpc/feign-client")
     public String callFeignClient() {
-        return rpcService.callFeignClient();
+        return rpcService.callFeignClient() + ". Host: " + getLocalHost();
     }
 
     @GetMapping(value = "/api/async/message")
     public String postMessage(@RequestParam String setting) {
         asyncService.postMessage(setting);
-        return "message was send, check the bar service";
+        return "message was send, check the bar service. Host: " + getLocalHost();
+    }
+
+    private String getLocalHost() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown host";
+        }
     }
 
 }
